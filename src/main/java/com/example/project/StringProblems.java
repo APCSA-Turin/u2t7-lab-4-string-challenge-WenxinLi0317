107a665bec03cdee2ea8a150be32bad4546d0cab
WenxinLi0317 <wenxinl8@nycstudents.net>
2024-10-10 21:39:19 +0000

    sd

--- src/main/java/com/example/project/StringProblems.java
+++ src/main/java/com/example/project/StringProblems.java
@@ -1,5 +1,5 @@
 package com.example.project;
-
+//concat,withoout,friz string
 public class StringProblems{
     //empty constructor
     public StringProblems(){}
@@ -11,11 +11,16 @@ public class StringProblems{
     // endsLy("oddy") → false
     public boolean endsLy(String x){
             //implement code here
-        int lastTwoLetter = (x.length()-2);
-        if (x.substring(lastTwoLetter).equals("ly")){
-            return true;
-        }
+            
+       if (x.length()<2){
         return false;
+       }
+        String lastTwo = x.substring((x.length()-2));
+        if (lastTwo.equals("ly")){
+            return true;
+        }else {
+            return false;
+        }
     }
 
 
@@ -23,34 +28,44 @@ public class StringProblems{
     // and return the result. However, if the concatenation creates a double-char, 
     // then omit one of the chars, so "abc" and "cat" yields "abcat".
     // conCat("abc", "cat") → "abcat"
+    //.        012.   345
+
     // conCat("dog", "cat") → "dogcat"
     // conCat("abc", "") → "abc"
+
     public String conCat(String s1, String s2){
         //implement code here
         Boolean bool = false;
-        String concatenation =  s1 + s2;
-        while(bool=false){
+        String concatenation = s1+s2;
+        int index1 = 0;
+        
+        int index2 = 1;
+        
+        int index3 = 2;
+
+        while(index3 <= concatenation.length()){
             
-            int index1 = 1;
-            int index2 = 2;
-            String sub1 = concatenation.substring(0,index1);
-            String sub2 = concatenation.substring(index1,index2);
+            
+            String sub1 = concatenation.substring(index1,index2);
+                                                  //2,3=c
+            String sub2 = concatenation.substring(index2,index3);//3,4=c
 
             if(sub1.equals(sub2)){
-                String sub3 = concatenation.substring(0,index1);
-                String sub4 = concatenation.substring(index2+1);
-
+                String sub3 = concatenation.substring(0,index1 );
+               ///
+                String sub4 = concatenation.substring(index2);
+                    //abc cat. 0-2,3
                 return sub3+sub4;
             }
             else{
                 index1 ++;
                 index2 ++;
+                index3 ++;
             }
-            if (index2> (concatenation.length()+1)){
-                return s1+s2;
-            }
+            
+            
         }
-        
+        return concatenation;
     }
 
     // Given a string, return a version without the first 2 chars. 
@@ -88,14 +103,14 @@ public class StringProblems{
     // withoutX("Hxix") → "Hxi"
     public String withoutX(String s1){
         String firstLetter = s1.substring(0,1);
-        String lastLetetr = s1.substring(s1.length() - 1);
+        String lastLetetr = s1.substring((s1.length()) - 1);
         int length = s1.length();
         if(firstLetter.equals("x") && lastLetetr.equals("x")){
-            s1 = s1.substring(1,(length));
+            s1 = s1.substring(1,(length-1));
         }else if (firstLetter.equals("x")){
             s1 = s1.substring(1);
         }else if (lastLetetr.equals("x")){
-            s1 = s1.substring(0,length);
+            s1 = s1.substring(0,length-1);
         }
         return s1;
     }
@@ -105,11 +120,12 @@ public class StringProblems{
     //  conditions are true, return "FizzBuzz". In all other cases, return the 
     //  string unchanged. (See also: FizzBuzz Code)
     // fizzString("fig") → "Fizz"
+
     // fizzString("dib") → "Buzz"
     // fizzString("fib") → "FizzBuzz"
     public String fizzString(String s1){
         String firstLtter = s1.substring(0,1);
-        String lastLetter = s1.substring(s1.length());
+        String lastLetter = s1.substring(s1.length()-1);
         if(firstLtter.equals("f") && lastLetter.equals("b")){
             s1 = "FizzBuzz";
         }else if(firstLtter.equals("f")){
@@ -131,15 +147,22 @@ public class StringProblems{
     // fizzString2(2) → "2!"
     // fizzString2(3) → "Fizz!"
     public String fizzString2(int x){
-        if(x%3==0 && x%5==0){
-            return "FizzBuzz";
-        }else if (x%3==0){
-            return "Fizz";
-        }else if (x%5==0){
-            return "Buzz";
-        }else {
-            String newx = "(String)x";
+       
+        if (x < 3) {
+            String newx = "" +x;
+
             return newx + "!";
+            
+        }else if(x%3==0 && x%5==0){
+            return "FizzBuzz!";
+        }else if (x%3==0){
+            return "Fizz!";
+        }else if(x%5==0){
+            return "Buzz!";
+        }else {
+            String newx = "" +x+"!";
+
+            return newx ;
         }
         
         
